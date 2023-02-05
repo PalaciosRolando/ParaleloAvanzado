2ed71b806af39fb106bb21d6f559e529b3c22ce9
PalaciosRolando <65567672+PalaciosRolando@users.noreply.github.com>
2023-02-05 16:29:55 -0600

    Update kernel.cu

--- kernel.cu
+++ kernel.cu
@@ -2,3 +2,44 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+
+__global__ void pow(int *a, int *b, int *res){
+  int i = threadIdx.x;
+  res[i] = a[i] * b[i];
+}
+
+int main(){
+  
+  const int n = 3; 
+  int size = n * sizeof(n);
+  
+  int a[n] = { 1, 7, 1 );
+  int b[n] = { 4, 7, 1 );
+  int res[n] = { 0, 0, 0 );
+  
+  int* devA = 0;
+  int* devB = 0;
+  int* devRes = 0;              
+                
+  cudaMalloc((void**)&devA, size);
+  cudaMalloc((void**)&devB, size);
+  cudaMalloc((void**)&devRes, size);
+    
+  cudaMemcpy(devA, a, size, cudaMempcyHostToDevice);
+  cudaMemcpy(devB, b, size, cudaMempcyHostToDevice);
+  cudaMemcpy(devRes, res, size, cudaMempcyHostToDevice);
+  
+  pow << <1, n >> > (devA, devB, devRes);
+  cudaDeviceSynchronize();
+
+  cudaMemcpy(res, devRes, size, cudaMemcpyDeviceToHost); 
+
+  printf("{%d, %d, %d}", res[0], res[1], res[2]);
+  cudaDeviceReset();
+  
+  cudaFree(devA);
+  cudaFree(devB);
+  cudaFree(devRes);
+  
+  return 0;
+}
